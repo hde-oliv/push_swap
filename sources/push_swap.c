f57e34638b4e621346b76119ff451f662fcb61fa
Henrique Rocha <henriquerocha@tutanota.com>
2021-09-15 10:52:26 -0400

    refactor: list creation

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -1,25 +1,57 @@
 #include "libft.h"
 #include "push_swap.h"
 
-void	print_content(void *integer)
+void	print_content(void *str)
 {
-	write(1, integer, ft_strlen((char *)integer));
+	write(1, str, ft_strlen(str));
+	write(1, "\n", 1);
 }
 
-int	main(int argc, char **argv)
+t_list	*create_stack_a(int list_size, void **list, char mode)
 {
 	t_list	*first;
 	t_list	*node;
 	int		i;
 
-	node = ft_lstnew(*(argv + 1));
-	first = node;
-	i = 2;
-	while (i != argc)
+	if (mode == 'v')
 	{
-		node->next = ft_lstnew(*(argv + i));
+		node = ft_lstnew(*(list + 1));
+		i = 2;
+	}
+	else
+	{
+		node = ft_lstnew(*list);
+		i = 1;
+	}
+	first = node;
+	while (i != list_size)
+	{
+		node->next = ft_lstnew(*(list + i));
 		node = node->next;
 		i++;
 	}
-//	ft_lstiter(first, &print_content);
+	return (first);
+}
+
+
+int	main(int argc, char **argv)
+{
+	t_list	*stack_a;
+	char	**list;
+	int		i;
+	//t_list	*stack_b;
+
+	i = 0;
+	if (argc > 2)
+		stack_a = create_stack_a(argc, (void *)argv, 'v');
+	else if (argc == 2)
+	{
+		list = ft_split(*(argv + 1), ' ');
+		while (list[i++])
+			;
+		stack_a = create_stack_a(i - 1, (void *)list, 's');
+	}
+	else
+		exit(1);
+	ft_lstiter(stack_a, &print_content);
 }
