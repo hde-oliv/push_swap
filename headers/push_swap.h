6131905bf6fde1952cc2739141399f0115705b77
Henrique Rocha <henriquerocha@tutanota.com>
2021-11-03 22:26:11 -0300

    style: add headers

--- headers/push_swap.h
+++ headers/push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hde-oliv <hde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 22:25:20 by hde-oliv          #+#    #+#             */
+/*   Updated: 2021/11/03 22:25:21 by hde-oliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "libft.h"
@@ -10,6 +22,8 @@
 /* Instructions */
 void	sa(int **a);
 void	rra(int **a, int a_size);
+void	rrb(int **b, int b_size);
+void	rrr(int **a, int **b, int a_size, int b_size);
 
 /* Utils */
 void	shift_one_right(int **stack, int stack_size);
