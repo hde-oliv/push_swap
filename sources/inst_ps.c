0560424653d46b2ed7e5107562f08a18246c2f58
hde-oliv <henriquerocha@tutanota.com>
2021-11-10 20:32:00 -0300

    fix: s instructions

--- sources/inst_ps.c
+++ sources/inst_ps.c
@@ -6,50 +6,55 @@
 /*   By: hde-oliv <hde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 22:25:25 by hde-oliv          #+#    #+#             */
-/*   Updated: 2021/11/04 20:02:11 by hde-oliv         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:31:49 by hde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "push_swap.h"
 
-void	sa(int **a)
+void	sa(int **a, int a_size)
 {
 	int	tmp;
 
-	tmp = (*a)[1];
-	(*a)[1] = (*a)[0];
-	(*a)[0] = tmp;
-	write(1, "sa\n", 3);
-}
-
-void	sb(int **b)
-{
-	int	tmp;
-
-	tmp = (*b)[1];
-	(*b)[1] = (*b)[0];
-	(*b)[0] = tmp;
-	write(1, "sb\n", 3);
-}
-
-void	ss(int ***a, int ***b)
-{
-	int	*tmp;
-
-	if (!(*a)[1])
+	if (a_size > 1) 
 	{
 		tmp = (*a)[1];
 		(*a)[1] = (*a)[0];
-		(*a)[0] = (*a)[1];
+		(*a)[0] = tmp;
+		write(1, "sa\n", 3);
 	}
-	if (!(*b)[1])
+}
+
+void	sb(int **b, int b_size)
+{
+	int	tmp;
+
+	if (b_size > 1)
 	{
 		tmp = (*b)[1];
 		(*b)[1] = (*b)[0];
-		(*b)[0] = (*b)[1];
+		(*b)[0] = tmp;
+		write(1, "sb\n", 3);
 	}
-	(void)tmp;
+}
+
+void	ss(int **a, int **b, int a_size, int b_size)
+{
+	int	tmp;
+
+	if (a_size > 1) 
+	{
+		tmp = (*a)[1];
+		(*a)[1] = (*a)[0];
+		(*a)[0] = tmp;
+	}
+	if (b_size > 1)
+	{
+		tmp = (*b)[1];
+		(*b)[1] = (*b)[0];
+		(*b)[0] = tmp;
+	}
 	write(1, "ss\n", 3);
 }
 
