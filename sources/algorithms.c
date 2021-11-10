dcf01d01aae7aeb25df850a2d7d3e48c3df9e8c2
hde-oliv <henriquerocha@tutanota.com>
2021-11-10 20:56:52 -0300

    refactor: on dumb_sort

--- sources/algorithms.c
+++ sources/algorithms.c
@@ -6,7 +6,7 @@
 /*   By: hde-oliv <hde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:45:34 by hde-oliv          #+#    #+#             */
-/*   Updated: 2021/11/10 20:37:35 by hde-oliv         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:56:39 by hde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,9 @@ void	dumb_sort(int *a, int *b, int a_size, int b_size)
 {
 	int	lowest_index;
 
-	while (1)
+	while (!is_sorted(a, a_size))
 	{
 		lowest_index = find_lowest_number(a, a_size);
-		if (a_size == 1)
-		{
-			while (b_size != 0)
-				pa(&a, &b, &a_size, &b_size);
-			break ;
-		}
 		if (lowest_index == 0 && a_size != 1)
 			pb(&a, &b, &a_size, &b_size);
 		else if (lowest_index < a_size / 2)
@@ -32,6 +26,8 @@ void	dumb_sort(int *a, int *b, int a_size, int b_size)
 		else
 			rra(&a, a_size);
 	}
+	while (b_size != 0)
+		pa(&a, &b, &a_size, &b_size);
 }
 
 void	mini_sort(int *stack, int stack_size)
@@ -40,18 +36,18 @@ void	mini_sort(int *stack, int stack_size)
 		return ;
 	if (stack_size == 2)
 	{
-		sa(&stack);
+		sa(&stack, stack_size);
 		return ;
 	}
 	if (stack[1] > stack[0])
 	{
 		rra(&stack, stack_size);
-		sa(&stack);
+		sa(&stack, stack_size);
 	}
 	else
 	{
-		sa(&stack);
+		sa(&stack, stack_size);
 		rra(&stack, stack_size);
-		sa(&stack);
+		sa(&stack, stack_size);
 	}
 }
