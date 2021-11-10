70d674e24bf3a6b06891173824a656aba2b95a95
hde-oliv <henriquerocha@tutanota.com>
2021-11-10 20:43:48 -0300

    refactor: dumb_sort

--- sources/algorithms.c
+++ sources/algorithms.c
@@ -6,12 +6,11 @@
 /*   By: hde-oliv <hde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:45:34 by hde-oliv          #+#    #+#             */
-/*   Updated: 2021/11/04 20:43:04 by hde-oliv         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:37:35 by hde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
 
 void	dumb_sort(int *a, int *b, int a_size, int b_size)
 {
@@ -20,23 +19,18 @@ void	dumb_sort(int *a, int *b, int a_size, int b_size)
 	while (1)
 	{
 		lowest_index = find_lowest_number(a, a_size);
-		if (lowest_index == 0 && a_size != 1)
-			pb(&a, &b, &a_size, &b_size);
-		else if (lowest_index == a_size - 1 && a_size != 1)
+		if (a_size == 1)
 		{
-			ra(&a, a_size);
-			pb(&a, &b, &a_size, &b_size);
-		}
-		else if (a_size == 1)
-		{
-			printf("a size = %d, b size = %d\n", a_size, b_size);
-			print_stack(b, b_size);
 			while (b_size != 0)
 				pa(&a, &b, &a_size, &b_size);
 			break ;
 		}
-		else
+		if (lowest_index == 0 && a_size != 1)
+			pb(&a, &b, &a_size, &b_size);
+		else if (lowest_index < a_size / 2)
 			ra(&a, a_size);
+		else
+			rra(&a, a_size);
 	}
 }
 
