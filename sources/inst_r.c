c495639efbb12aa2e32d90f02a89c5951cc88a1d
hde-oliv <henriquerocha@tutanota.com>
2021-11-10 21:05:53 -0300

    fix: writing r instructions

--- sources/inst_r.c
+++ sources/inst_r.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   r_inst.c                                           :+:      :+:    :+:   */
+/*   inst_r.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hde-oliv <hde-oliv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 22:25:16 by hde-oliv          #+#    #+#             */
-/*   Updated: 2021/11/03 22:35:59 by hde-oliv         ###   ########.fr       */
+/*   Updated: 2021/11/10 21:05:07 by hde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ra(int **a, int a_size)
 	tmp = (*a)[0];
 	shift_one_left(a, a_size);
 	(*a)[a_size - 1] = tmp;
-	write(1, "ra\n", 4);
+	write(1, "ra\n", 3);
 }
 
 void	rb(int **b, int b_size)
@@ -30,7 +30,7 @@ void	rb(int **b, int b_size)
 	tmp = (*b)[0];
 	shift_one_left(b, b_size);
 	(*b)[b_size - 1] = tmp;
-	write(1, "rb\n", 4);
+	write(1, "rb\n", 3);
 }
 
 void	rr(int **a, int **b, int a_size, int b_size)
@@ -43,5 +43,5 @@ void	rr(int **a, int **b, int a_size, int b_size)
 	tmp = (*b)[0];
 	shift_one_left(b, b_size);
 	(*b)[b_size - 1] = tmp;
-	write(1, "rr\n", 4);
+	write(1, "rr\n", 3);
 }
