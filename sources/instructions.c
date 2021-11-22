83dee242baff3bfce4a667199fce99b30298595f
hde-oliv <henriquerocha@tutanota.com>
2021-11-22 09:17:32 -0300

    feat: complete instructions

--- sources/instructions.c
+++ sources/instructions.c
@@ -4,37 +4,37 @@ void	sa(t_ps *push)
 {
 	t_stack	*tmp;
 
-	if (!stack_size(push->a))
+	if (stack_size(push->a) < 2)
 		return ;
 	tmp = push->a->next;
 	push->a->next = push->a->next->next;
 	tmp->next = push->a;
 	push->a = tmp;
-	// TODO: write
+	write(1, "sa\n", 3);
 }
 
 void	pb(t_ps *push)
 {
 	t_stack	*tmp;
 
-	if (!stack_size(push->a))
+	if (stack_size(push->a) < 2)
 		return ;
 	tmp = push->a;
 	push->a = push->a->next;
 	stack_add_front(&(push->b), tmp);
-	// TODO: write
+	write(1, "pb\n", 3);
 }
 
 void	pa(t_ps *push)
 {
 	t_stack	*tmp;
 
-	if (!stack_size(push->b))
+	if (stack_size(push->b) < 2)
 		return ;
 	tmp = push->b;
 	push->b = push->b->next;
 	stack_add_front(&(push->b), tmp);
-	// TODO: write
+	write(1, "pa\n", 3);
 }
 
 void	ra(t_ps *push)
@@ -44,10 +44,19 @@ void	ra(t_ps *push)
 	tmp = push->a;
 	push->a = push->a->next;
 	stack_add_back(&(push->a), tmp);
-	// TODO: write
+	write(1, "ra\n", 3);
 }
 
 void	rra(t_ps *push)
 {
-	// TODO
+	t_stack	*i;
+	t_stack	*j;
+
+	i = push->a;
+	while (i->next->next != NULL)
+		i = i->next;
+	j = i->next;
+	i->next = NULL;
+	stack_add_front(&(push->a), j);
+	write(1, "rra\n", 3);
 }
